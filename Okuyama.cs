d2047ed99c014f86c7b87d34e14c7bb9b4993f5a
Ryota Kimura <csharp_ryoryo@outlook.jp>
2026-04-07 00:22:55 +0900

    処理の分離
    今までProgramクラスに豚汁生成と投稿を書いていたが，これでは再利用性が落ちるので，豚汁生成を別クラスにして読みやすくした．

---
+++ Okuyama.cs
@@ -0,0 +1,51 @@
+
+
+class Okuyama
+{
+    struct OkuyamaChar
+    {
+        public char c;
+        public int trueIndex;
+        public int falseIndex;
+
+        public OkuyamaChar(char c, int trueIndex, int falseIndex)
+        {
+            this.c = c;
+            this.trueIndex = trueIndex;
+            this.falseIndex = falseIndex;
+        }
+    }
+    /// <summary>
+    /// 文字一覧
+    /// 1/2の確率で抽選して，成功ならtrueIndexへ，失敗ならfalseIndexへ飛ぶ
+    /// -1で終了
+    /// </summary>
+    static OkuyamaChar[] okuyamas = 
+		{
+			new OkuyamaChar('奥',1,2),
+			new OkuyamaChar('山',2,0),
+			new OkuyamaChar('だ',3,2),
+			new OkuyamaChar('な',-1,0)
+		};
+    /// <summary>
+    /// 豚汁のかけらを作る
+    /// </summary>
+    /// <param name="rnd">グローバルに使うRandom</param>
+    /// <returns>豚汁のかけら</returns>
+	static string GenerateOkuyama(Random rnd)
+	{
+		int index = 0;
+		var sb = new StringBuilder(15);
+		while(true)
+		{
+			bool rndIsTrue = rnd.Next(0,256) < 128;
+			sb.Append(okuyamas[index].c);
+			index = rndIsTrue ? okuyamas[index].trueIndex : okuyamas[index].falseIndex;
+			if(index == -1)
+			{
+				break;
+			}
+		}
+		return sb.ToString();
+	}
+}
--- Program.cs
+++ Program.cs
@@ -11,69 +11,29 @@ class DiscordJson
 	}
 }
 
-struct OkuyamaChar
-{
-	public char c;
-	public int trueIndex;
-	public int falseIndex;
-
-	public OkuyamaChar(char c, int trueIndex, int falseIndex)
-	{
-		this.c = c;
-		this.trueIndex = trueIndex;
-		this.falseIndex = falseIndex;
-	}
-}
-
 class Program
 {
-	static OkuyamaChar[] okuyamas = 
-		{
-			new OkuyamaChar('奥',1,2),
-			new OkuyamaChar('山',2,0),
-			new OkuyamaChar('だ',3,2),
-			new OkuyamaChar('な',-1,0)
-		};
-	static string GenerateOkuyama(Random rnd)
-	{
-		int index = 0;
-		var sb = new StringBuilder(15);
-		while(true)
-		{
-			bool rndIsTrue = (rnd.Next(0,256) < 128);
-			sb.Append(okuyamas[index].c);
-			index = (rndIsTrue ? okuyamas[index].trueIndex : okuyamas[index].falseIndex);
-			if(index == -1)
-			{
-				break;
-			}
-		}
-		return sb.ToString();
-	}
-
 	public static void Main(string[] args)
 	{
 		Random rnd = new Random();
 
-		string[] okuyamas = new string[5];
-		for(int i = 0;i < 5;i++)
-		{
-			okuyamas[i] = GenerateOkuyama(rnd);
-		}
 		string message = @$"〈今日の奥山〉
-- {okuyamas[0]}
-- {okuyamas[1]}
-- {okuyamas[2]}
-- {okuyamas[3]}
-- {okuyamas[4]}
+- {GenerateOkuyama(rnd)}
+- {GenerateOkuyama(rnd)}
+- {GenerateOkuyama(rnd)}
+- {GenerateOkuyama(rnd)}
+- {GenerateOkuyama(rnd)}
 今日は世界豚汁デーなんだな，はー！";
 		Console.WriteLine(message);
+
+		// 投稿処理
 		var dj = new DiscordJson(message);
 		string jsonMsg = JsonSerializer.Serialize(dj);
 		using (var client = new HttpClient())  
 		{  
 			var content = new StringContent(jsonMsg, Encoding.UTF8, "application/json");  
 			var response = client.PostAsync(Secret.discordUrl, content).Result;  
+			Console.WriteLine(response);
 		}
 	}
 }
